344fd0cf4440670a9c282ca9ac75e5baa5ce838e
Azazel0p10 <azazelop1@outlook.com>
2025-06-17 23:12:01 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>FrameNova | Legal</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Documentación Legal de FrameNova</h1>
+  <ul>
+    <li><a href="terms.html">Términos de Servicio</a></li>
+    <li><a href="privacy.html">Política de Privacidad</a></li>
+  </ul>
+</body>
+</html>
